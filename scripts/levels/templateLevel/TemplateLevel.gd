35956358b88d4293b92c88f81fb83b141169676b
Guilherme <gui.crb0@gmail.com>
2022-05-08 19:44:14 -0300

    movement

---
+++ scripts/levels/templateLevel/TemplateLevel.gd
@@ -0,0 +1,4 @@
+extends Spatial
+
+func _process(_delta):
+	$Player.run()
---
+++ scripts/player/Player.gd
@@ -0,0 +1,26 @@
+extends KinematicBody
+
+onready var animationPlayer = $Animation/AnimationPlayer
+
+export var speed = 0.5
+
+var motion = Vector3.ZERO
+
+func _ready():
+	animationPlayer.play("idleSouth")
+
+func run():
+	if Input.is_action_pressed("north") and not Input.is_action_pressed("south"):
+		motion.z = -speed/2
+	elif Input.is_action_pressed("south") and not Input.is_action_pressed("north"):
+		motion.z = speed/2
+	else:
+		motion.z = 0
+	
+	if Input.is_action_pressed("east") and not Input.is_action_pressed("west"):
+		motion.x = speed
+	elif Input.is_action_pressed("west") and not Input.is_action_just_pressed("east"):
+		motion.x = -speed
+	else:
+		motion.x = 0
+	motion = move_and_slide(motion,Vector3.UP)
